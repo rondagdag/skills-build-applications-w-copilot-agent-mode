2b3662013b2f0958035000ae872d9efbf7ac5de8
Ron Dagdag <3879302+rondagdag@users.noreply.github.com>
2025-04-04 22:35:55 +0000

    Update settings to restrict allowed hosts and add API root view

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializers import UserSerializer, TeamSerializer, ActivitySerializer, LeaderboardSerializer, WorkoutSerializer
 from .models import User, Team, Activity, Leaderboard, Workout
 
@@ -21,3 +23,14 @@ class LeaderboardViewSet(viewsets.ModelViewSet):
 class WorkoutViewSet(viewsets.ModelViewSet):
     queryset = Workout.objects.all()
     serializer_class = WorkoutSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    base_url = 'https://supreme-waffle-8000.app.github.dev/'
+    return Response({
+        'users': base_url + 'api/users/?format=api',
+        'teams': base_url + 'api/teams/?format=api',
+        'activities': base_url + 'api/activities/?format=api',
+        'leaderboard': base_url + 'api/leaderboard/?format=api',
+        'workouts': base_url + 'api/workouts/?format=api'
+    })
